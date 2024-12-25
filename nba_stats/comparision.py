67878ba2165e15d21ef7639b9ff02253443db832
gazeng2004 <gazeng2019@gmail.com>
2024-12-24 21:56:28 -0800

    display

--- nba_stats/comparision.py
+++ nba_stats/comparision.py
@@ -1,6 +1,14 @@
 from nba_api.stats.endpoints import playercareerstats, commonplayerinfo
 import csv
 
+"""
+Want to incorporate height, weight, position into the nba stats 
+Want to create a tinker outlines different stats points that can be inputed and show what player matches their respective roles
+If possible, would want to produce a picture of each person
+
+Afterward, think of ways to deal with ways with BIG outliers 
+Should position be weighted more than other stats? How would I do that?
+"""
 index = {
     "id" :  0,
     "Age" : 1,
@@ -23,16 +31,46 @@ index = {
     "BLK": 18,
     "TOV": 19,
     "PF": 20,
-    "PTS": 21
+    "PTS": 21,
+    "Height": 22,
+    "Position": 23,
+    "Weight": 24
 }
 
-def weight(input_stat: dict, player_stats : list):
+def weight(input_stat: dict, player_stats : list) -> int:
     weight : int = 0
     for stat, value in input_stat.items():
-        weight += abs(float(player_stats[index[stat]]) - value)
+
+        if(stat == 'Position'):
+            pos: str = player_stats[index[stat]]
+            if(pos != value):
+                weight += position_weight(pos, value)
+        else:
+            weight += abs(float(player_stats[index[stat]]) - value)
     return weight
 
-   
+def position_weight(player_pos, input_pos) -> int:
+    player_list: list[str] = position_sep(player_pos)
+    input_list: list[str] = position_sep(input_pos)
+    
+    try:
+        for pos in player_list:
+            for pos2 in input_list:
+                if pos == pos2:
+                    return 5 
+        return 10
+    except:
+        return 10
+
+
+def position_sep(pos) -> list[str]:
+    if(len(pos) <= 7):
+        return [pos]
+    elif(pos[6] == '-'):
+        return ["Forward", "Guard"]
+    elif(pos[5] == '-'):
+        return ["Center", "Forward"]
+
 def old_reader(value : float, stat : str):
     dict = {}
     with open('2024-2025nbaStats.csv', mode = 'r') as file:
@@ -53,7 +91,7 @@ def reader(input_dict : dict):
 
     return dict[min(dict)]
 
-def id_reader(id):
+def id_reader(id: str) -> None:
     player_info = commonplayerinfo.CommonPlayerInfo(player_id = id)
     last_season = playercareerstats.PlayerCareerStats(player_id = id) 
     pdf = player_info.get_data_frames()[0]
@@ -61,18 +99,25 @@ def id_reader(id):
     print(pdf)
     print(cdf)
 
-def question_num():
+def question_num() -> int:
     try:
         input_value = float(input("Please type a number: "))
     except:
         return question_num()
     return input_value
 
-def question():
+def question_str(string: str) -> str:
+    input_value = str(input("Please type a string: "))
+    return input_value
+
+def question() -> dict:
     input_dict = {}
     input_string = input("Please type a stat: ")
     while input_string in index:
-        input_value = question_num()
+        if(input_string == "Position"):
+            input_value = question_str("string")
+        else:
+            input_value = question_num()
         input_dict[input_string] = input_value
         input_string = input("Please type a stat: ")
     return input_dict
--- widget.py
+++ widget.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from nba_stats import comparision
 
 
 # This is a font that we'll use on all of the buttons, so we'll define it
@@ -10,6 +11,7 @@ DEFAULT_FONT_SMALL = ('Helvetica', 8)
 class SimpleLayoutApplication:
     def __init__(self):
         self._root = tk.Tk()
+        self._root.geometry("900x400")
 
         self.pts = tk.DoubleVar(value = 0.0)
         self.reb = tk.DoubleVar(value = 0.0)
@@ -18,7 +20,73 @@ class SimpleLayoutApplication:
         self.height = tk.IntVar(value = 0)
         self.position = tk.StringVar(value = "")
         self.weight = tk.IntVar(value = 0)
+        self.display = tk.StringVar(value = "Input Stats")
 
+        self._label_maker()
+        index = self._entry_maker()
+        self._pos_entry_make(index)
+        self._button_maker_command("Input", 2, 3, self.info_getter())
+        self._display = tk.Label(
+            self._root, 
+            height = 10, 
+            width = 60, 
+            font = DEFAULT_FONT, 
+            textvariable = self.display,
+            bg = "lightblue",
+            highlightthickness = 2,
+            highlightbackground = "black",
+        )
+        self._display.grid(row = 4, column = 0, columnspan = 6)
+
+    def _button_maker_command(self, text, row, column, command) -> tk.Button:
+        button = tk.Button(
+            master = self._root,
+            text = text, 
+            font = DEFAULT_FONT,
+            commander = command
+        )
+
+        button.grid(
+            row = row, column = column,
+            sticky = tk.N + tk.S + tk.W + tk.E
+        )
+        
+        return button
+
+    def info_getter(self):
+        stat_list = {}
+        if self.check(self.pts.get()):
+            stat_list["PTS"] = self.pts.get()
+            self.pts.set(0.0)
+        if self.check(self.reb.get()):
+            stat_list["REB"] = self.reb.get()
+            self.reb.set(0.0)
+        if self.check(self.ast.get()):
+            stat_list["AST"] = self.ast.get()
+            self.ast.set(0.0)
+        if self.check(self.blk.get()):
+            stat_list["BLK"] = self.blk.get()
+            self.blk.set(0.0)
+        if self.check(self.height.get()):
+            stat_list["Height"] = self.height.get()
+            self.height.set(0)
+        if self.check(self.position.get()):
+            stat_list["Position"] = self.position.get()
+            self.pts.set("")
+        if self.check(self.weight.get()):
+            stat_list["Weight"] = self.weight.get()
+            self.pts.set(0)
+        
+        id = comparision.reader(stat_list)
+        print(id)
+        
+    def check(self, stat):
+        stat_set = {0.0, 0, ""}
+        if stat in stat_set:
+            return False
+        return True
+
+    def _label_maker(self) -> None:
         index: int = 0
         self._stats: list[str] = ["PTS", "REB", "AST", "BLK", "Height(inches)","Weight(lbs)", "Position"]
         for stat in self._stats:
@@ -26,7 +94,8 @@ class SimpleLayoutApplication:
 
             stat_label.grid(row = 0, column = index, padx = 0, pady = 0)
             index += 1
-
+    
+    def _entry_maker(self) -> int:
         index: int = 0
         self._stats_vars: list[tk.Variable] = [self.pts, self.reb, self.ast, self.blk, self.height, self.weight]
         for var in self._stats_vars:
@@ -34,7 +103,15 @@ class SimpleLayoutApplication:
 
             entry.grid(row = 1, column = index, padx = 0, pady = 0)
             index += 1
-        
+        return index
+
+    def _make_label(self, root : tk, text : str):
+        return tk.Label(root, text = text, font = DEFAULT_FONT)
+
+    def _make_entry(self, root : tk, var : tk.Variable):
+        return tk.Entry(root, textvariable = var, font = DEFAULT_FONT_SMALL)
+    
+    def _pos_entry_make(self, index):
         opition = [
             "Guard",
             "Forward-Guard",
@@ -45,15 +122,9 @@ class SimpleLayoutApplication:
         self.pos_entry = tk.OptionMenu(self._root, self.position, *opition)
         self.pos_entry.grid(row = 1, column = index, padx = 0, pady = 0)
 
-    def _make_label(self, root : tk, text : str):
-        return tk.Label(root, text = text, font = DEFAULT_FONT)
-
-    def _make_entry(self, root : tk, var : tk.Variable):
-        return tk.Entry(root, textvariable = var, font = DEFAULT_FONT_SMALL)
-
-
     def run(self):
         self._root.mainloop()
+
 
 
 
